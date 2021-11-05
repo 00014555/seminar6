d73174fd615f4dda3dbd5a14f73ca08e1ac70f23
00014555 <wiut00014555@gmail.com>
2021-11-05 15:23:44 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# def fancy_hello_world():
+#  fancy_string = """
+#  any long text
+# ───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+# ───█▒▒░░░░░░░░░▒▒█───
+# ────█░░█░░░░░█░░█────
+# ─▄▄──█░░░▀█▀░░░█──▄▄─
+# █░░█─▀▄░░░░░░░▄▀─█░░█
+# """
+#  return fancy_string
+#
+# print(fancy_hello_world())
+
+
+# def print_name():
+#     print(Nozima)
+#
+# def print_name_with_return():
+#     return "Nozima2"
+#
+# print_name()
+# print(print_name_with_return())
+
+
+# def absolute_value(x):
+#  if x < 0:
+#   return -x
+#  elif x > 0:
+#   return x
+#  elif x==0:
+#      return 0
+# print(absolute_value(0))
+
+
+# Task4
+
+# def compare(x,y):
+#     if x > y:
+#         return 1
+#     elif x == 1:
+#         return 0
+#     elif x < y:
+#         return -1
+#
+# compare(1,3)
